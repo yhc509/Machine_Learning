183ca6c424e7066b39d51bfd689c507c7092ac42
foDev-Jung <31362761+foDev-Jung@users.noreply.github.com>
2017-08-31 15:23:07 +0900

    Deep Nueral Network 학습
    코드 리뷰 및 코드 실행

--- TensorFlow Study/01. TensorFlow Basic/01.basic.py
+++ TensorFlow Study/01. TensorFlow Basic/01.basic.py
@@ -46,6 +46,7 @@ print (sess.run(W))
 print ("=== b ===")
 print (sess.run(b))
 print ("=== expr ===")
+
 # expr 수식에는 X 라는 입력값이 필요합니다.
 # 따라서 expr 실행시에는 이 변수에 대한 실제 입력값을 다음처럼 넣어줘야합니다.
 print (sess.run(expr, feed_dict={X: x_data}))
--- TensorFlow Study/02. Neural Network Basic/01.classification.py
+++ TensorFlow Study/02. Neural Network Basic/01.classification.py
@@ -40,37 +40,10 @@ b2 = tf.Variable(tf.zeros([3]),name="b2")
 L = tf.add(tf.matmul(X, W1), b1)
 # 가중치와 편향을 이용해 계산한 결과 값에
 # 텐서플로우에서 기본적으로 제공하는 활성화 함수인 ReLU 함수를 적용합니다.
-L = tf.nn.relu(L)
-
-# 최종적인 아웃풋을 계산합니다.
-# 히든레이어에 두번째 가중치 W2와 편향 b2를 적용하여 3개의 출력값을 만들어냅니다.
-model = tf.add(tf.matmul(L, W2), b2)
-# 마지막으로 softmax 함수를 이용하여 출력값을 사용하기 쉽게 만듭니다
-# softmax 함수는 다음처럼 결과값을 전체합이 1인 확률로 만들어주는 함수입니다.
-# 예) [8.04, 2.76, -6.52] -> [0.53 0.24 0.23]
-model = tf.nn.softmax(model)
-
-# 신경망을 최적화하기 위한 비용 함수를 작성합니다.
-# 각 개별 결과에 대한 합을 구한 뒤 평균을 내는 방식을 사용합니다.
-# 전체 합이 아닌, 개별 결과를 구한 뒤 평균을 내는 방식을 사용하기 위해 axis 옵션을 사용합니다.
-# axis 옵션이 없으면 -1.09 처럼 총합인 스칼라값으로 출력됩니다.
-#        Y         model         Y * tf.log(model)   reduce_sum(axis=1)
-# 예) [[1 0 0]  [[0.1 0.7 0.2]  -> [[-1.0  0    0]  -> [-1.0, -0.09]
-#     [0 1 0]]  [0.2 0.8 0.0]]     [ 0   -0.09 0]]
-# 즉, 이것은 예측값과 실제값 사이의 확률 분포의 차이를 비용으로 계산한 것이며,
-# 이것을 Cross-Entropy 라고 합니다.
-cost = tf.reduce_mean(-tf.reduce_sum(Y * tf.log(model), axis=1))
-
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
-train_op = optimizer.minimize(cost)
-
-
-################
-# 신경망 모델 학습 #
-################
+L = tf.nn.relu(L)ㄴ
 init = tf.global_variables_initializer()
 sess = tf.Session()
-sess.run(init)
+sess.run(init)ㄴ
 
 for step in xrange(100):
     sess.run(train_op, feed_dict={X: x_data, Y: y_data})
@@ -83,6 +56,7 @@ for step in xrange(100):
 # 결과 확인				 #
 # 0: 기타 1: 포유류, 2: 조류 #
 ##########################
+
 # tf.argmax: 예측값과 실제값의 행렬에서 tf.argmax 를 이용해 가장 큰 값을 가져옵니다.
 # 예) [[0 1 0] [1 0 0]] -> [2 1]
 #    [[0.2 0.7 0.1] [0.9 0.1 0.]] -> [2 1]
